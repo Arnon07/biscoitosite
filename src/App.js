c627d7df9d94ffbdd52723b32a1368ef792ac8a1
Arnon Nunes <arnonguimaraes@gmail.com>
2020-08-31 18:42:46 -0300

    site biscoito da sorte

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './stilo.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            textoFrase: ''
+        };
+
+        this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+        this.frases = ['O importante não é vencer todos os dias, mas lutar sempre.', 
+        'Maior que a tristeza de não haver vencido é a vergonha de não ter lutado!',
+        'É melhor conquistar a si mesmo do que vencer mil batalhas.',
+        'Quem ousou conquistar e saiu pra lutar, chega mais longe!',
+        'Enquanto houver vontade de lutar haverá esperança de vencer.',
+        'Difícil é ganhar um amigo em uma hora; fácil é ofendê-lo em um minuto.'];
+    }
+    
+    quebraBiscoito(){
+        let state = this.state;
+        let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+        state.textoFrase = '" ' + this.frases[numeroAleatorio] + ' "';
+        this.setState(state);
+    }
+
+   render(){
+        return(
+            <div className="conteiner">
+                <img src={require('./assets/biscoito.png')} className="img" />
+                <Botao nome="Abrir Biscoito" acaoBtn={this.quebraBiscoito}/>
+                <h3 className="textoFrase">{this.state.textoFrase}</h3>
+            </div>
+        );
+    }
+}
+
+class Botao extends Component{
+    render(){
+        return(
+            <div>
+                    <button onClick={this.props.acaoBtn} className="btn" >{this.props.nome}</button>
+            </div>
+        );
+    }
 }
 
 export default App;
